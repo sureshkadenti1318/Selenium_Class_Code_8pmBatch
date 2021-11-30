55c77a10590b8b88b34393e11776321aa12590fb
K.Suresh@wns.com-at-767661025020 <K.Suresh@wns.com>
2021-11-30 20:58:40 +0530

    secondcomit

--- Selenium_NewProjct_8pm/src/test/java/TestNG/DataProvider.java
+++ Selenium_NewProjct_8pm/src/test/java/TestNG/DataProvider.java
@@ -34,39 +34,6 @@ public class DataProvider {
 	@Test(invocationCount=2)
 	public void GoogleTest() throws IOException, InterruptedException {
 
-		 driver.get("http://demo.automationtesting.in/Register.html");
-
-		FileInputStream fl = new FileInputStream("C:\\Users\\u293327\\Desktop\\8pm_Selenium_Workspace\\Selenium_NewProjct_8pm\\Resources\\TestData_1.xlsx");
-
-	
-		XSSFWorkbook wb = new XSSFWorkbook(fl);
-
-		XSSFSheet sheet = wb.getSheet("Sheet1");
-
-		String FirstName = sheet.getRow(4).getCell(0).getStringCellValue();
-
-		String LatName = sheet.getRow(4).getCell(1).getStringCellValue();
-
-		
-		String email = sheet.getRow(5).getCell(0).getStringCellValue();
-		
-		System.out.println("----FirstName---" + FirstName);
-
-		System.out.println("----LatName---" + LatName);
-		
-		System.out.println("----email---" + email);
-
-
-		// sheet.getRow(3).getCell(0).getNumericCellValue();
-
-		
-		  driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys(FirstName);
-		
-		  Thread.sleep(2000);
-		  driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys(LatName);
-		  Thread.sleep(2000);
-		  
-		  driver.findElement(By.xpath("	//input[@type='email']")).sendKeys(email);
 		 
 
 		  
